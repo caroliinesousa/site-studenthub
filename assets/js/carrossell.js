932003d567ffbfdde063c7cb717e1b75a47e840a
Vitor Fontenele <vitorfontenele19@gmail.com>
2025-05-18 13:34:16 +0100

    Adicionando o carrossel de imagens na pagina inicial

---
+++ assets/js/carrossell.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+    
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove('active'));
+        
+        slides[index].classList.add('active');
+    }
+    
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+    
+    showSlide(currentSlide);
+    
+    setInterval(nextSlide, 3000);
+});
