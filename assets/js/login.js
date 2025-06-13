85358ec0bbab2c3fbc39543f5937d98b729cbf5e
Alexa <94009500+alienexa@users.noreply.github.com>
2025-06-13 23:12:06 +0100

    adiciona dados login

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+const enterSec = document.querySelector(".login-form")
+const registerSec = document.querySelector(".register-form")
+const btnToRegister = document.querySelector("#btn-goto-register")
+const btnToLogin = document.querySelector("#btn-return-login")
+
+btnToRegister.addEventListener("click",() => {
+    registerSec.style.display = "block"
+    enterSec.style.display = "none"
+})
+
+btnToLogin.addEventListener("click",() => {
+    enterSec.style.display = "block"
+    registerSec.style.display = "none"
+})
