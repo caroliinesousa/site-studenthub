1c00fa4b3193eb9309d0483185c9ef8656f7d1e3
Alexa <94009500+alienexa@users.noreply.github.com>
2025-06-15 20:43:54 +0100

    premite existencia da logica de registo de usuarios

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,4 @@
-import {dadosLogin} from "./dados-login.js"
+import { dadosLogin } from "./dados-login.js"
 
 //Register Validation
 const nameForm = document.querySelector("#name")
@@ -16,30 +16,45 @@ nifForm.addEventListener("input", function () {
     this.value = this.value.replace(/\D/g, "").slice(0, 9);
 });
 
-submitForm.addEventListener("click",(e) => {
+submitForm.addEventListener("click", (e) => {
     e.preventDefault()
     //regex para garantir que nif tem 9 numeros
     if (!/^\d{9}$/.test(nifForm.value)) {
         nifValidation.textContent = "O nif tem de ter 9 digitos"
+        return
     }
 
-    if (passwordForm !== confirmPasswordForm) {
+    if (passwordForm.value !== confirmPasswordForm.value) {
         passwordValidation.textContent = "As passwords têm de ser iguais"
+        return
     }
 
-    const user = { 
-        id:typeForm.value+nifForm.value,
-        name: nameForm.value, 
-        email: emailForm.value, 
+    const user = {
+        id: typeForm.value + nifForm.value,
+        name: nameForm.value,
+        email: emailForm.value,
         password: passwordForm.value
-    };    
-    
+    };
+
     const listUsers = localStorage.getItem('users');
-    const users = listUsers ? JSON.parse(listUsers): dadosLogin;
+    const users = listUsers ? JSON.parse(listUsers) : dadosLogin;
     users.push(user)
 
     localStorage.setItem('users', JSON.stringify(users));
-    console.log("usuario cadastrado:", user)
+
+    nameForm.value = ""
+    emailForm.value = ""
+    nifForm.value = ""
+    nifValidation.textContent = ""
+    passwordForm.value = ""
+    confirmPasswordForm.value = ""
+    passwordValidation.textContent = ""
+
+    const enterSec = document.querySelector(".login-form")
+    const registerSec = document.querySelector(".register-form")
+
+    enterSec.style.display = "block"
+    registerSec.style.display = "none"
 })
 
 
