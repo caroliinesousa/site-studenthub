25383b5ad928bda05432070330825a98a51c261e
Alexa <94009500+alienexa@users.noreply.github.com>
2025-06-15 20:42:34 +0100

    premite que o usuario faça login com sucesso

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,46 +15,26 @@ btnToLogin.addEventListener("click",() => {
     registerSec.style.display = "none"
 })
 
-//Register Validation
-const nameForm = document.querySelector("#name")
-const emailForm = document.querySelector("#email")
-const typeForm = document.querySelector("#user-type")
-const nifForm = document.querySelector("#user-number")
-const nifValidation = document.querySelector("#alert-nif")
-const passwordForm = document.querySelector("#new-password")
-const confirmPasswordForm = document.querySelector("#confirm-password")
-const submitForm = document.querySelector("#submit-register")
-const passwordValidation = document.querySelector("#alert-password")
+const emailLogin = document.querySelector("#username")
+const passwordLogin = document.querySelector("#password")
+const btnLogin = document.querySelector("#btn-login-form")
+const usersListString = localStorage.getItem("users")
+const usersList = usersListString ? JSON.parse(usersListString): dadosLogin;
+const erroLogin = document.querySelector("#erro-login")
+//console.log(usersList)
 
-//impede de digitar letras
-nifForm.addEventListener("input", function () {
-    this.value = this.value.replace(/\D/g, "").slice(0, 9);
-});
-
-submitForm.addEventListener("click",(e) => {
+btnLogin.addEventListener("click",(e) => {
     e.preventDefault()
-    //regex para garantir que nif tem 9 numeros
-    if (!/^\d{9}$/.test(nifForm.value)) {
-        nifValidation.textContent = "O nif tem de ter 9 digitos"
-    }
-
-    if (passwordForm !== confirmPasswordForm) {
-        passwordValidation.textContent = "As passwords têm de ser iguais"
-    }
-
-    const user = { 
-        id:typeForm.value+nifForm.value,
-        name: nameForm.value, 
-        email: emailForm.value, 
-        password: passwordForm.value
-    };    
-    
-    const listUsers = localStorage.getItem('users');
-    const users = listUsers ? JSON.parse(listUsers): dadosLogin;
-    users.push(user)
-
-    localStorage.setItem('users', JSON.stringify(users));
-    console.log("usuario cadastrado:", user)
+    usersList.forEach(user => {
+        if(user.email == emailLogin.value && user.password == passwordLogin.value){
+            localStorage.setItem('loginSession',JSON.stringify(user))
+            if(user.id.charAt(0)=='a'){
+                window.location.href = "./profile.html"
+            }else{
+                window.location.href = "./dashboard.html"
+            }
+        }else{
+            erroLogin.textContent = "Email ou Senha Incorretos"
+        }
+    });
 })
-
-// Lendo a string
