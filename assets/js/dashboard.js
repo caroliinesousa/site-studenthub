8f24941a16834a185df888526ac33cfac3a39c1e
Ana Sousa <caroliinesousa@hotmail.com>
2025-06-18 15:50:01 +0100

    Comentando js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,11 +1,14 @@
 import {dadosLogin} from "./dados-login.js"
-
-const tbody = document.querySelector("#tbody-user")
+// Seleciona o elemento <tbody> da tabela que possui o id "tbody-user"
+const tbody = document.querySelector("#tbody-user") 
+// Se houver dados no localStorage, converte de string para objeto (JSON)
+// Caso contrário, usa os dados importados de "dados-login.js"
 const userListString = localStorage.getItem("users")
 const userList = userListString ? JSON.parse(userListString):dadosLogin;
 
-
+// Percorre cada usuário na lista
 userList.forEach(user => {
+    // Adiciona uma nova linha na tabela com os dados do usuário
     tbody.innerHTML += `
     <tr>
         <td>${user.name}</td>
